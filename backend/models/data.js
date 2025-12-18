fa0ba16e161d2d697c1689a10cab830d0706b177
Airon Tark <airon@tark.pro>
2025-12-18 17:20:36 +0200

    data.js: fix the password hash in DB. The previous one doesn't work with "demo123"

--- backend/models/data.js
+++ backend/models/data.js
@@ -6,7 +6,7 @@ const users = [
   {
     id: 1,
     email: 'demo@careerontrack.ai',
-    password: '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', // password: "demo123"
+    password: '$2a$10$Mk546lq8HUezhZZemjQbxu/ZTmO.jflU0dXqzVVkAjHHVa4WDiAMu', // password: "demo123"
     name: 'Demo User',
     createdAt: new Date().toISOString()
   }
